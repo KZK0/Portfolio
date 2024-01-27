0b8a0df4641c7ec31ad23e7324e313a4c04042fd
KZKO <med62640@hotmail.fr>
2024-01-27 17:38:01 +0100

    Ajout animation dans le header, ajout de vantaJS, création de la banner

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from 'react';
+import Fog from 'vanta/src/vanta.fog.js';
+import './banner.scss';
+
+export const Banner = () => {
+    const vantaRef = useRef(null);
+
+    useEffect(() => {
+        // Initialise l'effet de brouillard sur l'élément avec la classe banner-section
+        vantaRef.current = Fog({
+            el: ".banner-section", // Sélectionne l'élément avec la classe banner-section
+            color: 0x0, // Couleur du brouillard
+            highlightColor: 0x0, // Couleur de mise en évidence (optionnel)
+            midtoneColor: 0x0, // Couleur médiane (optionnel)
+            lowlightColor: 0x0, // Couleur basse (optionnel)
+            baseColor: 0x595959, // Couleur de base (optionnel)
+            blurFactor: 0.9, // Facteur de flou (optionnel)
+            speed: 1, // Vitesse de l'animation (optionnel)
+            zoom: 0.6 // Zoom (optionnel)
+            // Vous pouvez ajuster ces valeurs selon vos préférences
+        });
+
+        // Nettoie l'effet de brouillard lors du démontage du composant
+        return () => {
+            if (vantaRef.current) vantaRef.current.destroy();
+        };
+    }, []); // Cette fonction s'exécute une seule fois après le premier rendu
+
+    return (
+        <section className='banner-section'>
+            <div className='banner-bloc'>
+                <p>Hi, im KZKO and im a Front-End develloper</p>
+            </div>
+        </section>
+    );
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 import { Header } from '../../components/header/header'
+import { Banner } from '../../components/banner/banner';
 import './home.scss'
 
 export const Index = () => {
     return (
         <div>
             <Header />
+            <Banner />
         </div>
     );
 };
